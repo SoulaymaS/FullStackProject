b24c021a378250daf3ac2a6ae08d3b4b5e0148a5
Hendbsila <102285340+Hendbsila@users.noreply.github.com>
2022-08-31 21:15:16 +0100

    navbarUpdate

--- appPff-main/src/app/components/formation-list/formation-list.component.ts
+++ appPff-main/src/app/components/formation-list/formation-list.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormationService } from './../../services/formation.service';
 
 
 @Component({
@@ -9,35 +7,11 @@ import { FormationService } from './../../services/formation.service';
   styleUrls: ['./formation-list.component.css']
 })
 export class FormationListComponent implements OnInit {
-  listofFormation;
-  constructor(private formServ: FormationService, private router:Router){}
-  ngOnInit(): void {
-    this.formServ.getFormations().subscribe({
-      next: (res)=> {
-        this.listofFormation= res;
-        console.log(res);
-      }, error: (err) =>{
-        console.log(err);
-      }
-    });
-  }
-  SupprimerFormation(id){
-    
-      if (confirm("etes vous sur de vouloir supprimer la formation ?")){
-      
-        this.formServ.deleteFormation(id).subscribe({
-          next:(res)=>{
-            console.log(res);
-            this.router.navigateByUrl('/formations');
-          },
-          error: (err) => {
-            console.log('probleme avec supprimer formation')
-          }
-    
-        })
-  
 
-      }
-    }
+  constructor(){}
+  ngOnInit(): void {
+   
+  }
+
   }
 
--- appPff-main/src/app/components/session-list/session-list.component.ts
+++ appPff-main/src/app/components/session-list/session-list.component.ts
@@ -9,39 +9,14 @@ import { SessionServiceService } from 'src/app/services/session-service.service'
   styleUrls: ['./session-list.component.css']
 })
 export class SessionListComponent implements OnInit {
-  listofsessions
-  listFormation
-  constructor( private SessionServ:SessionServiceService, private router:Router) { }
+ 
+  constructor() { }
 
   ngOnInit(): void {
-        this.SessionServ.getSessions().subscribe({
-      next: (res)=> {
-        this.listofsessions= res;
-        console.log(res);
-      }, error: (err) =>{
-        console.log(err);
-      }
-    });
+      
   
   }
 
 
-  SupprimerSession(id){
-    
-    if (confirm("etes vous sur de vouloir supprimer la session ?")){
-    
-      this.SessionServ.deleteSession(id).subscribe({
-        next:(res)=>{
-          console.log(res);
-          this.router.navigateByUrl('/sessions');
-        },
-        error: (err) => {
-          console.log('probleme avec supprimer session')
-        }
-  
-      })
 
-
-    }
-  }
 }
